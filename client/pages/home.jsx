96b31af71841b1254c800d84ec7f29504ae321e0
Alex-Davis0 <amchops@comcast.net>
2022-02-21 19:49:12 -0800

    to save the cats and jail time

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavBar from '../components/nav-bar';
-
+import Carousel from '../components/carousel';
 export default function Home(props) {
   return (
     <>
     <NavBar />
+    <Carousel />
     </>
   );
 }
